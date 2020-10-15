1b531cea77ed1eb72cf25dacbcca8b5c2e80ada3
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-15 23:57:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash=={}
+    return nil
+    lowest_key=""
+    lowest_value=""
+    
+  end
 
 end
