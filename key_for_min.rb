27d6841d662618b475a0a80da1bba6b87c5ef468
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-16 00:16:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,11 +8,11 @@ def key_for_min_value(name_hash)
     lowest_key=nil #default key
     lowest_value=nil #default value
     name_hash.each do |key,value|
-      if lowest_value==nil
-         lowest_key=key
+      if lowest_value==nil #which it is
+         lowest_key=key #default assignments
          lowest_value=value
       elsif (value<lowest_value)
-            lowest_key=key
+            lowest_key=key #new assignsments as iteration goes on
             lowest_value=value
       end
 
